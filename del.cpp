8739c62aa1aa94f9e1f979f5ed2af791e662b1f3
Nalin <kumarnalinsingh@gmail.com>
2023-09-08 16:58:08 +0530

    adding del.cpp

---
+++ del.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int> v;
+    v.push_back(45);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(10);
+    cout<<v[2]<<"\n";
+
+
+
+}
